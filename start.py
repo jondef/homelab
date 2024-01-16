ce1b66af49422f99d5db90cfdc37836a42d23cff
Jon Defilla <jdefilla@gmail.com>
2024-01-16 18:42:13 +0100

    update start.py

--- start.py
+++ start.py
@@ -58,7 +58,10 @@ def handle_commands(args):
         enabled_svc = get_enabled_services()
         print("Starting services: " + ", ".join(enabled_svc))
 
-        compose_args = [f"-f ./services/{svc}/docker-compose.yml" for svc in enabled_svc] + ['up', '-d', '--remove-orphans']
+        compose_args = ['docker-compose']
+        for svc in enabled_svc: compose_args += ['-f', f'./services/{svc}/docker-compose.yml']
+        compose_args +=['up', '-d', '--remove-orphans']
+
         run_docker_compose(compose_args)
 
 
